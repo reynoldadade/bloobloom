b1068d2916ade5d710c1811a65d098d4d88a9a18
reynoldadade <reynoldadade@gmail.com>
2022-09-09 13:06:21 +0000

    setup getters and actions to call store on change

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosInstance, AxiosRequestHeaders } from "axios";
 import { Collections } from "../models/collections.model";
+import { GlassesResponse } from "../models/glasses.model";
 const instance: AxiosInstance = axios.create({
   baseURL:
     "https://staging-api.bloobloom.com/user/v1/sales_channels/website/collections",
@@ -9,6 +10,17 @@ const instance: AxiosInstance = axios.create({
 export async function GET_Collections(): Promise<Collections[] | null> {
   try {
     const response = await instance.get("");
+    return response.data;
+  } catch (error) {
+    return null;
+  }
+}
+
+export async function GET_glasses(
+  url: string
+): Promise<GlassesResponse | null> {
+  try {
+    const response = await instance.get(url);
     return response.data;
   } catch (error) {
     return null;
--- src/stores/collections.ts
+++ src/stores/collections.ts
@@ -1,10 +1,12 @@
 import { defineStore } from "pinia";
-import { Ref, ref, computed, ComputedRef } from "vue";
-import { Glass } from "../models/glasses.model";
+import { Ref, ref, computed, ComputedRef, watch } from "vue";
+import { Glass, GlassesResponse } from "../models/glasses.model";
+import { GET_glasses } from "../services/api";
 
 export const useCollectionsStore = defineStore("collections", () => {
   // will hold glasses array
   const glasses: Ref<Glass[]> = ref([]);
+  const totalCountOfGlasses: Ref<number> = ref(0);
   // page number of the current page
   const pageNumber: Ref<number> = ref(1);
   // color of glasses selected in filter
@@ -101,5 +103,70 @@ export const useCollectionsStore = defineStore("collections", () => {
     return `${collectionName.value}/glasses/?sort[type]=${sortType}&sort[order]=${sortOrder}&page[limit]=${pageLimit}&page[number]=${pageNumber.value}&filters[filter_frame_variant_home_trial_available]=${filter_frame_variant_home_trial_available}${colorConfigurationParamString.value}${frameConfigurationParamString.value}${lensVariantPrescriptionParamString.value}${lensVariantTypesParamString}`;
   });
 
-  return { glassesRequestURL };
+  // watch page number
+  watch(
+    () => pageNumber.value,
+    async (newVal, oldVal) => {
+      if (newVal > oldVal) {
+        // since it the page number that changed we need to append to current
+        const response: GlassesResponse | null = await GET_glasses(
+          glassesRequestURL.value
+        );
+        if (response) {
+          // we need to append the new data to the older one
+          glasses.value = [...glasses.value, ...response.glasses];
+        }
+      }
+    }
+  );
+
+  // watch filters_glass_variant_frame_variant_colour_tag_configuration_names
+  watch(
+    () =>
+      filters_glass_variant_frame_variant_colour_tag_configuration_names.value,
+    async (newVal, oldVal) => {
+      if (newVal !== oldVal) {
+        pageNumber.value = 1;
+        await getGlasses();
+      }
+    }
+  );
+  // watch filters_glass_variant_frame_variant_colour_tag_configuration_names
+  watch(
+    () =>
+      filters_glass_variant_frame_variant_frame_tag_configuration_names.value,
+    async (newVal, oldVal) => {
+      // we need to reset page number because we might miss out of the first page if the page number is higher than 1
+      if (newVal !== oldVal) {
+        pageNumber.value = 1;
+        await getGlasses();
+      }
+    }
+  );
+  // have to watch colleciton name because we might need to change the type of collection
+  watch(
+    () => collectionName.value,
+    async (newVal, oldVal) => {
+      if (oldVal !== newVal) {
+        pageNumber.value = 1;
+        await getGlasses();
+      }
+    }
+  );
+
+  // action to request glasses
+  async function getGlasses() {
+    const response: GlassesResponse | null = await GET_glasses(
+      glassesRequestURL.value
+    );
+    if (response) {
+      glasses.value = response.glasses;
+      totalCountOfGlasses.value = response.meta.total_count;
+    } else {
+      glasses.value = [];
+      totalCountOfGlasses.value = 0;
+    }
+  }
+
+  return { glasses, totalCountOfGlasses, getGlasses };
 });
